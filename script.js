ddb17f98165ab98039d431cb85f0f1697b62f7b8
Daniel <87285520+dannielnotfound@users.noreply.github.com>
2022-02-21 20:04:29 -0300

    Animações
    Implementação das animações: transitions, keyframes; correção de bugs

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const menuList = document.getElementById("menu-list");
 menuList.style.maxHeight = "0px";
 console.log(menuList);
-
-
-
 function showMenu() {
   if (menuList.style.maxHeight == "0px") {
     menuList.style.maxHeight = "110px";
   } else {
     menuList.style.maxHeight = "0px";
   }
+}
+function showArrow() {
+  const img = document.getElementById("imgDisplay");
+  img.style.display = "block";
+}
+function hideArrow() {
+  const img = document.getElementById("imgDisplay");
+  img.style.display = "none";
 }
